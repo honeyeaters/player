7ffad965f451c1e88ff2cfb93bf056126361af5c
honeyeaters <honey.eaters.dev@gmail.com>
2022-06-17 01:43:27 +0900

    init project

---
+++ app/src/main/java/com/honeyeaters/player/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.honeyeaters.player.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.honeyeaters.player.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ player/src/main/java/com/honeyeaters/player/Player.kt
@@ -0,0 +1,15 @@
+package com.honeyeaters.player
+
+import android.util.Log
+
+object Player {
+
+    private val TAG = "Player"
+
+    /**
+     * 테스트 로깅
+     */
+    fun play() {
+        Log.d(TAG, "play.")
+    }
+}

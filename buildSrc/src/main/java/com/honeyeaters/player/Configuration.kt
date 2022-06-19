165b339346ecbea900994fa770332f48236b9530
honeyeaters <honey.eaters.dev@gmail.com>
2022-06-19 13:42:32 +0900

    update version to 1.0.0

---
+++ buildSrc/src/main/java/com/honeyeaters/player/Configuration.kt
@@ -0,0 +1,20 @@
+package com.honeyeaters.player
+
+object Configuration {
+
+    object Project {
+        const val group = "com.honeyeaters.player"
+        const val version = "1.0.0"
+    }
+
+    object Android {
+        const val compileSdk = 31
+        const val buildTools = "31.0.0"
+        const val minSdk = 23
+        const val targetSdk = 31
+    }
+
+    object Version {
+        const val exoPlayer = "2.16.1"
+    }
+}
